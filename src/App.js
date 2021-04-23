f4bd7c0c1797733b7ac5dcf7d4ebdedb0d3f37d2
ZarNiHein <zarnihein2312@gmail.com>
2021-04-23 22:09:13 +0630

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import { React, Component } from 'react';
+import { Card, Col, Container, Nav, Row } from 'react-bootstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends Component {
 
-export default App;
+  state = {
+    isLoading: true,
+    streamList: []
+  }
+
+  componentDidMount() {
+    this.loadFetch();
+    this.setState({ isLoading: false });
+  }
+
+  loadFetch = async () => {
+    await fetch("https://www.scorebat.com/video-api/v1")
+      .then(data => data.json())
+      .then(streamList => this.setState({ streamList }))
+      .catch(e => console.log(e));
+  }
+
+
+  render() {
+    return (
+      <div className="App" >
+        {
+          !this.state.isLoading && (
+            <Container>
+              <h1 className="p-5 font-weight-bold">Zack-Foot Streaming App</h1>
+              <Row className="justify-content-center">
+                {this.state.streamList.map((data, index) => (
+                  <Col className="p-2" xs="12" sm="6" md="4" key={index}>
+                    <Card className="cardStyle m-auto" style={{ cursor: "pointer" }}>
+                      <Card.Link href={data.url} target="blank" rel="noreferrer">
+                        <Card.Img variant="top" src={data.thumbnail} alt={data.title} />
+                        <Card.Body className="text-center">
+                          <Card.Title className="font-weight-bold">{data.title}</Card.Title>
+                        </Card.Body>
+                      </Card.Link>
+                    </Card>
+                  </Col>
+                ))}
+              </Row>
+            </Container>)
+        }
+      </div >
+    );
+  }
+};
